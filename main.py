1191ef9aa2de532223cab93929eefe138d3627f0
milad <seyeadmiladnouri@gmail.com>
2026-02-10 23:37:41 +0330

    Yolo-test

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ model = YOLO("model/yolov10x.pt")
 
 model = YOLO("model/yolov8x.pt")
  5 persons, 22 cars, 1 truck, 1 traffic light, 519.7ms Speed: 1.7ms
+
+
+results = model.predict(source=path)
+results[0].show()
+print(results[0].names)
+
+endregion
+
+region Step 2 ...
+
+path = "images/sampleImage.jpg"
+model = YOLO("yolov8n.pt")
+results = model.predict(source=path, classes=[2])
+results[0].show()
